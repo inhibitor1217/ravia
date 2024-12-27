af1bc17cffc390ca60cc96233fe87b2ddeb7cc41
dugi <dugi@channel.io>
2024-12-27 14:20:47 +0900

    feat: setup boot

--- crates/ravia_engine/src/lib.rs
+++ crates/ravia_engine/src/lib.rs
@@ -1,1 +1,11 @@
+/// Engine name.
+pub const ENGINE_NAME: &str = "ravia_engine";
 
+/// Engine version.
+pub const ENGINE_VERSION: &str = "0.1.0";
+
+/// Starts the engine.
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
+pub fn boot() {
+    log::info!(target: "ravia_engine", "Booting {} {}", ENGINE_NAME, ENGINE_VERSION);
+}
--- examples/ravia_hello_triangle/src/main.rs
+++ examples/ravia_hello_triangle/src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    ravia_engine::boot();
 }
