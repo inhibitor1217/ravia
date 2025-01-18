59cb7ef34cf87acf1402be690462d59248d28672
dugi <dugi@channel.io>
2025-01-18 21:42:52 +0900

    feat(ravia_engine): ensure Resource impl Component

--- crates/ravia_engine/src/resource/internal/resource.rs
+++ crates/ravia_engine/src/resource/internal/resource.rs
@@ -1,3 +1,5 @@
+use crate::ecs;
+
 /// An external resource dynamically loaded from the filesystem.
 #[derive(Debug, Clone, PartialEq)]
 pub struct Resource {
@@ -10,6 +12,8 @@ impl Resource {
         Self { path }
     }
 }
+
+assert_impl_all!(Resource: ecs::storage::Component);
 
 impl std::fmt::Display for Resource {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
